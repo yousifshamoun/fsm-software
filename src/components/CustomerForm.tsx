429afd2f9bb66e68fabb7566b06834544d02a3e4
yousifshamoun <yousif2002@berkeley.edu>
2022-11-21 22:17:03 -0800

    customer info

--- src/components/CustomerForm.tsx
+++ src/components/CustomerForm.tsx
@@ -2,31 +2,72 @@ import React from "react";
 
 const CustomerForm = () => {
     return (
-        <div className="flex justify-center items-center">
-            <form className="w-full max-w-3xl">
+        <div className="flex justify-center items-center mt-10">
+            <form className="w-full max-w-4xl">
                 <p className="text-left font-bold text-xl my-2">
                     Customer Information
                 </p>
-                <div className="flex flex-wrap -mx-3 mb-2">
-                    <div className="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+                <div className="flex flex-wrap -mx-2 mb-2">
+                    <div className="w-full md:w-1/4 px-2 mb-6 md:mb-0">
                         <input
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-city"
+                            className="appearance-none block w-full bg-white border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="name"
                             type="text"
-                            placeholder="Albuquerque"
+                            placeholder="Name"
                         />
                     </div>
-                    <div className="w-full md:w-1/5 px-3 mb-6 md:mb-0">
+                    <div className="w-full md:w-1/4 px-2 mb-6 md:mb-0">
+                        <input
+                            className="appearance-none block w-full bg-white border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="street-address"
+                            type="text"
+                            placeholder="Street"
+                        />
+                    </div>
+
+                    <div className="w-full md:w-1/12 px-1 mb-6 md:mb-0">
+                        <input
+                            className="appearance-none block w-full bg-white border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id=""
+                            type="text"
+                            placeholder="Unit"
+                        />
+                    </div>
+                    <div className="w-full md:w-1/6 px-2 mb-6 md:mb-0">
+                        <input
+                            className="appearance-none block w-full bg-white border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="grid-zip"
+                            type="text"
+                            placeholder="Zip"
+                        />
+                    </div>
+                    <div className="w-full md:w-1/4 px-2 mb-6 md:mb-0">
+                        <input
+                            className="appearance-none block w-full bg-white border border-gray-400 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                            id="phone"
+                            type="tel"
+                            placeholder="Phone"
+                        />
+                    </div>
+                </div>
+                <p className="text-left font-bold text-xl my-4">Job Details</p>
+                <div className="flex flex-wrap mb-2">Hel</div>
+            </form>
+        </div>
+    );
+};
+{
+    /* <div className="w-full md:w-1/5 px-2 mb-6 md:mb-0">
                         <div className="relative">
                             <select
-                                className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                className="block appearance-none w-full bg-white border border-gray-400 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                                 id="grid-state"
                             >
                                 <option>New Mexico</option>
                                 <option>Missouri</option>
                                 <option>Texas</option>
                             </select>
-                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-200">
                                 <svg
                                     className="fill-current h-4 w-4"
                                     xmlns="http://www.w3.org/2000/svg"
@@ -36,35 +77,6 @@ const CustomerForm = () => {
                                 </svg>
                             </div>
                         </div>
-                    </div>
-                    <div className="w-full md:w-1/5 px-3 mb-6 md:mb-0">
-                        <input
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-zip"
-                            type="text"
-                            placeholder="90210"
-                        />
-                    </div>
-                    <div className="w-full md:w-1/5 px-3 mb-6 md:mb-0">
-                        <input
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-zip"
-                            type="text"
-                            placeholder="90210"
-                        />
-                    </div>
-                    <div className="w-full md:w-1/5 px-3 mb-6 md:mb-0">
-                        <input
-                            className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                            id="grid-zip"
-                            type="text"
-                            placeholder="90210"
-                        />
-                    </div>
-                </div>
-            </form>
-        </div>
-    );
-};
-
+                    </div> */
+}
 export default CustomerForm;
